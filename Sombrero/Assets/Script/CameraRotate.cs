f88c741d6e0d71759c4590c931cf605221dbcad9
paul.jeanbour <pa.jeanbourquin@gmail.com>
2018-04-20 13:46:32 +0200

    Initial Commit

---
+++ Sombrero/Assets/Script/CameraRotate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TowerOrientation {
+    FRONT = 0,
+    LEFT = 1,
+    BACK = 2,
+    RIGHT = 3
+}
+public class CameraRotate : MonoBehaviour {
+
+    // Use this for initialization
+    private TowerOrientation orientation;
+    public TowerOrientation Orientation {
+        get {
+            return orientation;
+        }
+    } 
+    
+	void Start () {
+        orientation = 0;
+	}
+
+    private bool isRotating = false;
+    public int speed = 10;
+    private float angleRotated;
+    private float previousAngleRotate;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !isRotating) {
+            angleRotated = 0;
+            isRotating = true;
+        }
+        if (isRotating) {
+            angleRotated += 90 * Time.deltaTime * speed;
+            this.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 1, 0), (angleRotated<90) ? 90 * Time.deltaTime * speed : 90-previousAngleRotate );
+            if (angleRotated >= 90) {
+                isRotating = false;
+                orientation = (TowerOrientation)((int)(orientation + 1) % 4);
+            }
+            previousAngleRotate = angleRotated;
+        }
+	}
+}
---
+++ Sombrero/Assets/Script/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float speed;
+    private bool isJump;
+	void Start () {
+        this.speed = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rigb = this.GetComponent<Rigidbody>();
+        CameraRotate camRot = FindObjectOfType<CameraRotate>();
+        if (Input.GetKey(KeyCode.A)) {
+            if(camRot.Orientation == TowerOrientation.FRONT) rigb.MovePosition(transform.position + new  Vector3(-speed, 0.0f, 0.0f) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.LEFT) rigb.MovePosition(transform.position + new Vector3(0, 0, speed) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.RIGHT) rigb.MovePosition(transform.position + new Vector3(0, 0, -speed) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.BACK) rigb.MovePosition(transform.position + new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            if (camRot.Orientation == TowerOrientation.FRONT) rigb.MovePosition(transform.position + new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.LEFT) rigb.MovePosition(transform.position + new Vector3(0, 0, -speed) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.RIGHT) rigb.MovePosition(transform.position + new Vector3(0, 0, speed) * Time.deltaTime);
+            else if (camRot.Orientation == TowerOrientation.BACK) rigb.MovePosition(transform.position + new Vector3(-speed, 0.0f, 0.0f) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!isJump) {
+                isJump = true;
+                rigb.AddForce(new Vector3(0, 300, 0));
+            }
+        }
+        if (rigb.velocity == Vector3.zero) {
+            isJump = false;
+        }
+    }
+}
